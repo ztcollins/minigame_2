f59a1edf83d55026b7062c5eb624895c2d5526b4
Sambhav Satija <darkryder@users.noreply.github.com>
2024-02-17 21:46:41 -0600

    PaperMover: go into rage mode depending on dog's excitment level. Randomize multiple things. Do tick based math

--- Assets/PaperMover.cs
+++ Assets/PaperMover.cs
@@ -3,92 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-class Doggo
+struct DoggoPhaseAttributes
 {
-
-    Dictionary<DoggoPhase, DoggoPhaseAttributes> mPhases;
-    DoggoPhaseAttributes currPhase;
-
-    float phaseStartedAt;
-    float currPhaseLimit;
-
-    System.Random rnd;
-
-    public Doggo(Dictionary<DoggoPhase, DoggoPhaseAttributes> phases, DoggoPhase init, float time)
-    {
-        mPhases = phases;
-        rnd = new System.Random();
-
-        phaseStartedAt = time;
-        currPhase = mPhases[init];
-
-        float min = currPhase.stayRange.Item1;
-        float max = currPhase.stayRange.Item2;
-        currPhaseLimit = (float)rnd.NextDouble() * (max - min) + min;
-        
-    }
-
-    public enum DoggoPhase
-    {
-        DISTRACTED, // can only go to playful
-        PLAYFUL, // can go to either distracted or beast.
-        BEAST // can only go to playful.
-    };
-
-    public struct DoggoPhaseAttributes
-    {
-        public DoggoPhase phase;
-        public float pEnter;
-        public int damage;
-        public (float, float) stayRange;
-
-        public float pullSpeedFactor;
-        public float shakeDampingFactor;
-    }
-
-    public void step(float timeNow)
-    {
-        float elapsed = timeNow - phaseStartedAt;
-        if (elapsed < currPhaseLimit) return;
-
-        if (currPhase.phase == DoggoPhase.DISTRACTED)
-        {
-            currPhase = mPhases[DoggoPhase.PLAYFUL];
-        } else if (currPhase.phase == DoggoPhase.BEAST)
-        {
-            currPhase = mPhases[DoggoPhase.PLAYFUL];
-        }
-        else
-        {
-            // toss coin.
-            float pBeast = mPhases[DoggoPhase.BEAST].pEnter;
-            float coin = (float) rnd.NextDouble();
-            if (coin < pBeast)
-            {
-                currPhase = mPhases[DoggoPhase.BEAST];
-            } else
-            {
-                currPhase = mPhases[DoggoPhase.DISTRACTED];
-            }
-        }
-
-
-        phaseStartedAt = timeNow;
-        float min = currPhase.stayRange.Item1;
-        float max = currPhase.stayRange.Item2;
-        currPhaseLimit = (float)rnd.NextDouble() * (max - min) + min;
-    }
-
-    public int getDamage()
-    {
-        return currPhase.damage;
-    }
-
-    public DoggoPhaseAttributes getCurrPhase()
-    {
-        return currPhase;
-    }
+    public float pullSpeedFactor;
+    public float shakeDampingFactor;
 }
+
+enum DoggoPhase
+{
+    DISTRACTED, // can only go to playful
+    PLAYFUL, // can go to either distracted or beast.
+    BEAST // can only go to playful.
+};
 
 public class PaperMover : MonoBehaviour
 {
@@ -96,57 +22,228 @@ public class PaperMover : MonoBehaviour
     [SerializeField] float amount = 1.0f; //how much it shakes
     Vector3 startPos;
 
-    int paperDurability = 100;
-    Doggo dog;
+    [SerializeField] float playerPull = -0.008f; // how much paper is pulled when player presses space.
+    [SerializeField] float dogPull = 0.003f;
+
+    [SerializeField] float dogExcitmentAddScale = 0.01f; // how much dog excitement is added everytime space is pressed.
+    [SerializeField] float dogExcitmentReductionScale = -0.01f; // how much dog excitment drops everytime space is not pressed. 2 is to 1 means it drops twice as quickly as it builds up. 
+
+    // TODO: Excitment can increase if player keeps space pressed for a long time.
+    // Completed: Allow 50ms delay in beast mode before destroying heart.
+    // Complete: once player enters phase, make it difficult to exit, by reducing threshold by random amount. so they can't play around it.
+
+    struct LevelAttributes
+    {
+        public float dogExcitedThreshold; // hidden meter. Beyond this, the dog will enter beast mode.
+    
+        public DoggoPhaseAttributes[] dogPhaseAttributes;
+
+        // every so often, a random number will be picked between range.
+        // the range will dynamically lerp to that number. (NOT DOING THIS RN). We immediately update it.
+        // when it reaches that number, it will stay there for some time.
+        // then a new number will be picked.
+        public (int, int) excitedThresholdRanges_forLevel;
+        public float timeToChangeThreshold_seconds; // not being used
+        public float timeToStayAtThreshold_seconds;
+
+        public float gracePeriodBeastDamage;
+    }
+
+    struct StateAttributes
+    {
+        public int playerHearts;
+        public float lastHealthHitWhen;
+        public float dogExcitment; // hidden meter. how excited is the dog currently?
+        public DoggoPhase dogPhase;
+        public float nextThresholdChange; // at what time should we start changing the threshold;
+        public float whenExitDistracted; // if distracted, when to exit the mode. Should be set when distracted is enabled.
+
+        public float lastPhysicsTick; // instead of increasing dog excitment every frame, we should do it at fixed timestemps.
+    }
+
+    LevelAttributes lvl;
+    StateAttributes state;
+    System.Random rnd;
+
+    void Lvl1()
+    {
+        lvl = new LevelAttributes
+        {
+            dogExcitedThreshold = 100,
+            excitedThresholdRanges_forLevel = (0, 0),
+            timeToChangeThreshold_seconds = 2,
+            timeToStayAtThreshold_seconds = 2,
+            dogPhaseAttributes = new DoggoPhaseAttributes[] {
+                new DoggoPhaseAttributes { pullSpeedFactor = 0f, shakeDampingFactor = 0f }, // distracted
+                new DoggoPhaseAttributes { pullSpeedFactor = 1.2f, shakeDampingFactor = 0.1f }, // playful
+                new DoggoPhaseAttributes { pullSpeedFactor = 0.4f, shakeDampingFactor = 0.8f }, // beast
+            },
+            gracePeriodBeastDamage = 0.2f,
+        };
+
+        state = new StateAttributes
+        {
+            dogExcitment = 0,
+            playerHearts = 5,
+            lastHealthHitWhen = Time.time - 5,
+            dogPhase = DoggoPhase.DISTRACTED,
+            nextThresholdChange = Time.time + lvl.timeToChangeThreshold_seconds,
+            whenExitDistracted = Time.time + 100000,
+        };
+    }
+
+    // TODO: we can add these for each level.
+    void Lvl2()
+    {
+        lvl = new LevelAttributes
+        {
+            dogExcitedThreshold = 50,
+            excitedThresholdRanges_forLevel = (60, 80),
+            timeToChangeThreshold_seconds = 2,
+            timeToStayAtThreshold_seconds = 2,
+            dogPhaseAttributes = new DoggoPhaseAttributes[] {
+                new DoggoPhaseAttributes { pullSpeedFactor = 0f, shakeDampingFactor = 0f }, // distracted
+                new DoggoPhaseAttributes { pullSpeedFactor = 1.2f, shakeDampingFactor = 0.1f }, // playful
+                new DoggoPhaseAttributes { pullSpeedFactor = 0.4f, shakeDampingFactor = 0.8f }, // beast
+            },
+            gracePeriodBeastDamage = 0.2f,
+        };
+
+        state = new StateAttributes
+        {
+            dogExcitment = 20,
+            playerHearts = 5,
+            lastHealthHitWhen = Time.time - 5,
+            dogPhase = DoggoPhase.PLAYFUL,
+            nextThresholdChange = Time.time + lvl.timeToChangeThreshold_seconds,
+            whenExitDistracted = 0, // set from external function call when we trigger a distracted state.
+        };
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         startPos = this.transform.position;
+        rnd = new System.Random();
 
-        Dictionary<Doggo.DoggoPhase, Doggo.DoggoPhaseAttributes> phases = new Dictionary<Doggo.DoggoPhase, Doggo.DoggoPhaseAttributes>();
-        phases[Doggo.DoggoPhase.DISTRACTED] = new Doggo.DoggoPhaseAttributes { phase = Doggo.DoggoPhase.DISTRACTED, damage = 0, pEnter = 0.5f, stayRange = (0.5f, 1.5f), pullSpeedFactor = 0f, shakeDampingFactor = 0f, };
-        phases[Doggo.DoggoPhase.PLAYFUL] = new Doggo.DoggoPhaseAttributes { phase = Doggo.DoggoPhase.PLAYFUL, damage = 3, pEnter = 1f, stayRange = (1, 3), pullSpeedFactor = 1f, shakeDampingFactor=0.5f,};
-        phases[Doggo.DoggoPhase.BEAST] = new Doggo.DoggoPhaseAttributes { phase = Doggo.DoggoPhase.BEAST, damage = 15, pEnter = 0.5f, stayRange = (1, 2), pullSpeedFactor = 0.5f, shakeDampingFactor=1f,};
+        lvl = new LevelAttributes();
+        state = new StateAttributes();
 
-        dog = new Doggo(phases, Doggo.DoggoPhase.DISTRACTED, Time.time);
-        lastHealthHitWhen = Time.time;
+        Lvl2();
+        //Lvl1();
     }
 
-    float lastHealthHitWhen;
-    const float healthHitOftenSeconds = 0.3f;
+    float since(float when)
+    {
+        return Time.time - when;
+    }
+
+    int phaseIdx(DoggoPhase phase)
+    {
+        if (phase == DoggoPhase.DISTRACTED) return 0;
+        if (phase == DoggoPhase.PLAYFUL) return 1;
+        return 2;
+    }
 
     // Update is called once per frame
+
+    int iFrame = 0;
     void Update()
     {
-        dog.step(Time.time);
-        Doggo.DoggoPhaseAttributes currAttrs = dog.getCurrPhase();
-
-        float xShake = startPos.x + Mathf.Sin(Time.time * speed) * currAttrs.shakeDampingFactor;
-        float dogPull = 0.005f * currAttrs.pullSpeedFactor;
-        float playerPull = -0.01f;
-
-        if (Time.time - lastHealthHitWhen > healthHitOftenSeconds)
+        iFrame += 1;
+        if (iFrame % 10 == 0)
         {
-            lastHealthHitWhen = Time.time;
-            paperDurability -= dog.getDamage();
-            //Debug.Log("Paper health: " + paperDurability);
+            Debug.Log("Health:" + state.playerHearts + " threshold:" + lvl.dogExcitedThreshold + " excitment:" + state.dogExcitment + " phase:" + state.dogPhase);
+        }
+
+        DoggoPhaseAttributes dogAttrs = lvl.dogPhaseAttributes[phaseIdx(state.dogPhase)];
+
+        // have the dog pull the paper based on its phase.
+        float currZ = transform.position.z;
+        currZ += dogPull * dogAttrs.pullSpeedFactor;
+        float xShake = startPos.x + Mathf.Sin(Time.time * speed) * dogAttrs.shakeDampingFactor;
+
+        bool tick = since(state.lastPhysicsTick) > 0.01 ? true : false;
+        if (tick)
+        {
+            state.lastPhysicsTick = Time.time;
+        }
+
+        bool spacePressed = Input.GetKey(KeyCode.Space);
+        if (!spacePressed) {
+            if (tick && state.dogPhase != DoggoPhase.DISTRACTED) {
+                state.dogExcitment += dogExcitmentReductionScale;
+                state.dogExcitment = Mathf.Clamp(state.dogExcitment, 0, 200);
+            }
+
+            // player successfully escapes the mode.
+            if (state.dogPhase == DoggoPhase.BEAST && state.dogExcitment < lvl.dogExcitedThreshold)
+            {
+                state.dogPhase = DoggoPhase.PLAYFUL;
+
+                // give players breathing room.
+                int min = 4;
+                int max = 14;
+                state.dogExcitment -= (float)rnd.NextDouble() * (max - min) + min;
+            }
+
+            transform.position = new Vector3(xShake, 0, currZ);
+            return;
+        }
+
+        // space Pressed!
+        currZ += playerPull;
+        if (state.dogPhase == DoggoPhase.BEAST && since(state.lastHealthHitWhen) > lvl.gracePeriodBeastDamage)
+        {
+            state.playerHearts -= 1;
+            state.lastHealthHitWhen = Time.time;
+            Debug.Log("Ouch! Health decreased. Hearts: " + state.playerHearts);
         }
 
         //end game  
-        if(paperDurability <= 0) {
+        if (state.playerHearts == 0)
+        {
             FindObjectOfType<GameManager>().EndGame();
+            return;
         }
-
-
-        float currZ = transform.position.z;
-        currZ += dogPull;
-
-        if(Input.GetKey(KeyCode.Space)) {
-            currZ += playerPull;
-        }
-
         transform.position = new Vector3(xShake,0,currZ);
-        
+
+        if (tick && state.dogPhase != DoggoPhase.DISTRACTED)
+        {
+            state.dogExcitment += dogExcitmentAddScale;
+            state.dogExcitment = Mathf.Clamp(state.dogExcitment, 0, 200);
+        }
+
+        if (state.dogPhase == DoggoPhase.PLAYFUL && state.dogExcitment > lvl.dogExcitedThreshold)
+        {
+            state.dogPhase = DoggoPhase.BEAST;
+
+            // immediately increase dogExcitment by some amount, so player has to suffer for few frames before getting it back.
+            int min = 80;
+            int max = 130;
+            state.dogExcitment += (float)rnd.NextDouble() * (max - min) + min;
+            state.dogExcitment = Mathf.Clamp(state.dogExcitment, 0, 200);
+
+            state.lastHealthHitWhen = Time.time; // give them 0.5s to react.
+        }
+
+        if (state.dogPhase == DoggoPhase.PLAYFUL && state.dogExcitment * 1.2 > lvl.dogExcitedThreshold)
+        {
+            Debug.Log("About to enter beast mode!");
+        }
+
+        if (state.dogPhase == DoggoPhase.DISTRACTED && since(state.whenExitDistracted) > 0)
+        {
+            state.dogPhase = DoggoPhase.PLAYFUL;
+        }
+
+        // perhaps unnecessary?
+        if (state.nextThresholdChange - Time.time < 0)
+        {
+            int min = lvl.excitedThresholdRanges_forLevel.Item1;
+            int max = lvl.excitedThresholdRanges_forLevel.Item2;
+            lvl.dogExcitedThreshold = (float)rnd.NextDouble() * (max - min) + min;
+            state.nextThresholdChange = Time.time + lvl.timeToStayAtThreshold_seconds;
+        }
     }
 }
